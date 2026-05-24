9066cf3722b0de7a0cd65ff3d3e005adce1796f5
tamzid-h <112463809+tamzid-h@users.noreply.github.com>
2026-05-24 10:52:11 +1000

    Update NumberPiece.cs

---
+++ Games/NumericalTicTacToe/NumberPiece.cs
@@ -0,0 +1,18 @@
+namespace BoardGameFramework
+{
+    public class NumberPiece : Piece
+    {
+        public int Value { get; }
+
+        public NumberPiece(int ownerId, int value)
+            : base(ownerId)
+        {
+            Value = value;
+        }
+
+        public override string Symbol
+        {
+            get { return Value.ToString(); }
+        }
+    }
+}
